cfffa8700c918d116faa8cb5b8e580ba8ff54be6
drey012 <cleytonteamoo@gmail.com>
2025-03-29 20:54:02 -0300

    Ambiente configurado

--- SigMa/app/index.jsx
+++ SigMa/app/index.jsx
@@ -2,13 +2,16 @@ import React from "react";
 import { useColorScheme, View } from "react-native";
 import { DarkTheme, NavigationContainer } from "@react-navigation/native";
 import { useState, useEffect } from "react";
+import { NavigationIndependentTree } from "@react-navigation/native";
 
 export default function App() {
     const colorScheme = useColorScheme();
 
     return (
+        <NavigationIndependentTree>
         <NavigationContainer theme={colorScheme === "dark" ? DarkTheme : null}>
-            
+            oi
         </NavigationContainer>
+        </NavigationIndependentTree>
     );
 }
